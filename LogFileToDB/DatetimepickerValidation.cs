10427781f7c626b53a9ca948408066596550139e
Kaya Kopp <k.kopp@neogeo.com>
2022-11-20 19:14:12 +0100

    adds DatetimePicker Validation Class

---
+++ LogFileToDB/DatetimepickerValidation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+
+namespace LogFileToDB
+{
+    public class DatetimepickerValidation : ValidationRule
+    {
+        _
+        public DatetimepickerValidation(): base()
+        {
+
+        }
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
