160bb057cf780d79952bc93481564c1545e2f2db
Kaya Kopp <k.kopp@neogeo.com>
2022-12-09 08:59:55 +0100

    paramless constructor

--- LogFileToDB/ListFilterControles.xaml.cs
+++ LogFileToDB/ListFilterControles.xaml.cs
@@ -7,11 +7,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LogFileToDB
 {
@@ -21,26 +16,22 @@ namespace LogFileToDB
     public partial class ListFilterControles : UserControl
     {
         public event EventHandler FilterSelected;
-        private readonly QueryRepository queryRepository;
-        public ListFilterControles(QueryRepository queryRepository)
+
+        public ListFilterControles()
         {
             InitializeComponent();
-            this.queryRepository = queryRepository;
         }
 
         private void FillComboBoxes()
         {
-
         }
 
         private void MethodePicker_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
         }
 
         private void StatusPicker_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
         }
 
         //private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
